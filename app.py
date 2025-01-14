833da481805f16b717f9acee20baaaafeee73a5e
Hardik Sharma <115365148+hardiksharmmaaaa@users.noreply.github.com>
2025-01-14 22:46:49 +0530

    finance agent

---
+++ app.py
@@ -0,0 +1,36 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+
+#making the Web Search Agent 
+web_Search_agent = Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include the source from where you parse the information"],
+    show_tool_calls=True,
+    markdown=True
+)
+
+#making the finance agent 
+finance_agent = Agent(
+    name="Finance Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True, #it will show what all tools are there
+    markdown=True
+)
+
+
+# Combining both the agents to make MultiModal 
+multi_ai_agent=Agent(
+    team=[web_Search_agent,finance_agent],
+    model=Groq(id="llama-3.1-70b-versatile"),
+    instructions=["Always include sources", "Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+multi_ai_agent.print_response("Summarize analyst recommendations and share the latest news for NVDA", stream=True)
