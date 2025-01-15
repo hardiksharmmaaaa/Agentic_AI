986e7f764a9be4a77466df7733c291770141afd7
Hardik Sharma <115365148+hardiksharmmaaaa@users.noreply.github.com>
2025-01-15 22:07:00 +0530

    Playground Added

--- playground.py
+++ playground.py
@@ -2,35 +2,46 @@ from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
 
-#making the Web Search Agent 
+import os 
+from phi.playground import Playground, serve_playground_app
+
+load_dotenv()
+
+# Create the Web Search Agent
 web_Search_agent = Agent(
     name="Web Search Agent",
     role="Search the web for the information",
-    model=Groq(id="llama-3.1-70b-versatile"),
+    model=Groq(id="llama-3.1-8b-instant"),
     tools=[DuckDuckGo()],
     instructions=["Always include the source from where you parse the information"],
     show_tool_calls=True,
     markdown=True
 )
 
-#making the finance agent 
+# Create the Finance Agent
 finance_agent = Agent(
     name="Finance Agent",
-    model=Groq(id="llama-3.1-70b-versatile"),
+    model=Groq(id="llama-3.1-8b-instant"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
     instructions=["Use tables to display data"],
-    show_tool_calls=True, #it will show what all tools are there
-    markdown=True
+    show_tool_calls=True,
+    markdown=True,
 )
 
-
-# Combining both the agents to make MultiModal 
-multi_ai_agent=Agent(
-    team=[web_Search_agent,finance_agent],
-    model=Groq(id="llama-3.1-70b-versatile"),
+# Combining both agents into a MultiModal Agent
+multi_ai_agent = Agent(
+    team=[web_Search_agent, finance_agent],
+    model=Groq(id="llama-3.1-8b-instant"),
     instructions=["Always include sources", "Use tables to display data"],
     show_tool_calls=True,
     markdown=True,
 )
-multi_ai_agent.print_response("Summarize analyst recommendations and share the latest news for NVDA", stream=True)
+multi_ai_agent.print_response("share the latest news for NVDA", stream=True)
+
+# Initialize Playground properly
+apps = Playground(agents=[finance_agent, web_Search_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("playground:apps", reload=True)
